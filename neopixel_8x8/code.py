8aab426f3440ef6b6ba42a8599b1602c75e05242
Pete Richardson <pete@peterichardson.com>
2024-08-05 13:33:15 -0700

    neopixel_8x8: (minor) tweak comment & whitespace

--- neopixel_8x8/code.py
+++ neopixel_8x8/code.py
@@ -6,11 +6,12 @@ import neopixel
 import time
 
 num_pixels = 8 * 8
-pixels = neopixel.NeoPixel(board.GP19, num_pixels)
-pixels.brightness = 0.5    # all pixels are on in this version, so reduce brightness
-loop_delay = 0.02   # time between color changes.  0.01 is too fast.  0.5 is too slow
+loop_delay = 0.02  # color refresh rate. 0.01 = too fast.  0.05 = too slow
 
-# starting color 
+pixels = neopixel.NeoPixel(board.GP19, num_pixels)
+pixels.brightness = 0.5  # reduce from 1.0 b/c all pixels are on
+
+# starting color
 r = 128
 g = 0
 b = 128
@@ -29,7 +30,7 @@ while True:
         rdir = -rdir
     if r > 255:
         r = 255
-        rdir = -rdir  
+        rdir = -rdir
 
     g = g + gdir * 3
     if g < 0:
@@ -37,7 +38,7 @@ while True:
         gdir = -gdir
     if g > 255:
         g = 255
-        gdir = -gdir 
+        gdir = -gdir
 
     b = (b + bdir * 5)
     if b < 0:
@@ -45,7 +46,7 @@ while True:
         bdir = -bdir
     if b > 255:
         b = 255
-        bdir = -bdir 
+        bdir = -bdir
 
     # print("(r, g, b): ", (r, g, b))
     pixels.fill((r, g, b))
